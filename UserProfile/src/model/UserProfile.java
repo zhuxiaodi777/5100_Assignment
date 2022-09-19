01de5a7cc2f6816c6c7a276fe854918b30ab3ce4
zhuxiaodi777 <zhu.xiaod@northeastern.edu>
2022-09-19 17:53:55 -0400

    UserProfile/

---
+++ UserProfile/src/model/UserProfile.java
@@ -0,0 +1,203 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author judy
+ */
+public class UserProfile {
+    
+    private String first_name;
+    private String last_name;
+    private String age;
+    private String dob;
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSt1() {
+        return st1;
+    }
+
+    public void setSt1(String st1) {
+        this.st1 = st1;
+    }
+
+    public String getSt2() {
+        return st2;
+    }
+
+    public void setSt2(String st2) {
+        this.st2 = st2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAff() {
+        return aff;
+    }
+
+    public void setAff(String aff) {
+        this.aff = aff;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getCareer() {
+        return career;
+    }
+
+    public void setCareer(String career) {
+        this.career = career;
+    }
+
+    public String getD1() {
+        return d1;
+    }
+
+    public void setD1(String d1) {
+        this.d1 = d1;
+    }
+
+    public String getD1s() {
+        return d1s;
+    }
+
+    public void setD1s(String d1s) {
+        this.d1s = d1s;
+    }
+
+    public String getD1e() {
+        return d1e;
+    }
+
+    public void setD1e(String d1e) {
+        this.d1e = d1e;
+    }
+
+    public String getD2() {
+        return d2;
+    }
+
+    public void setD2(String d2) {
+        this.d2 = d2;
+    }
+
+    public String getD2s() {
+        return d2s;
+    }
+
+    public void setD2s(String d2s) {
+        this.d2s = d2s;
+    }
+
+    public String getD2e() {
+        return d2e;
+    }
+
+//    Telephone number
+//Email
+//Street Line 1
+//Street Line 2
+//City
+//Country
+//Affiliation
+//Major
+//Career
+//Degree 1
+//Degree 1 Start
+//Degree 1 End
+//Degree 2
+//Degree 2 Start
+//Degree 2 End
+//Photo
+    public void setD2e(String d2e) {
+        this.d2e = d2e;
+    }
+    private String no;
+    private String email;
+    private String st1;
+    private String st2;
+    private String city;
+    private String country;
+    private String aff;
+    private String major;
+    private String career;
+    private String d1;
+    private String d1s;
+    private String d1e;
+    private String d2;
+    private String d2s;
+    private String d2e;
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+    
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    
+}
